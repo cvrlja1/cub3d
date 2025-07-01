ab9c08469aeb5c62a0184c7de17216da54a91669
Thomas Lueghammer <tluegham@c2r1p8.42vienna.com>
2025-07-01 14:32:35 +0200

    fixed norminette error

--- srcs/parsing/get_map_infos.c
+++ srcs/parsing/get_map_infos.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_map_infos.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nicvrlja <nicvrlja@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: tluegham <tluegham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 23:01:25 by nightcore         #+#    #+#             */
-/*   Updated: 2025/07/01 12:41:47 by nicvrlja         ###   ########.fr       */
+/*   Updated: 2025/07/01 14:31:11 by tluegham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,9 @@ static bool	skip_pre_map_whitespace(int fd, char *buf, int *bytes_read)
 	ssize_t	bytes;
 
 	nl_offset = 0;
-	read(fd, buf, 1);
+	bytes = read(fd, buf, 1);
+	if (bytes == -1)
+		return (false);
 	while (is_whitespace(buf[0]))
 	{
 		nl_offset++;
@@ -60,33 +62,41 @@ static bool	skip_pre_map_whitespace(int fd, char *buf, int *bytes_read)
 	return (true);
 }
 
+static bool	try_read_map_line(int fd, char *buf, t_map_info *mi)
+{
+	ssize_t	bytes_read;
+	int		i;
+
+	i = 0;
+	while (buf[0] != '\n')
+	{
+		if (!is_accepted_char(buf[0], mi))
+			return (false);
+		if (++i > mi->y)
+			mi->y = i;
+		bytes_read = read(fd, buf, 1);
+		if (bytes_read == 0)
+			break ;
+		if (bytes_read == -1)
+			return (print_error(READ_FD_ERR), false);
+	}
+	if (i == 0)
+		return (print_error(MAP_NL_ERR), false);
+	return (true);
+}
+
 t_map_info	get_map_infos(int fd, int *bytes_read)
 {
 	t_map_info	mi;
-	int			i;
 	char		buf[1];
-	ssize_t		bytes;
 
 	if (!skip_pre_map_whitespace(fd, buf, bytes_read))
 		return ((t_map_info){-1, -1, NULL, -1});
 	mi = (t_map_info){.x = 0, .y = 0, .spawn_count = 0};
 	while (1)
 	{
-		i = 0;
-		while (buf[0] != '\n')
-		{
-			if (!is_accepted_char(buf[0], &mi))
-				return ((t_map_info){.x = 0, .y = 0});
-			if (++i > mi.y)
-				mi.y = i;
-			bytes = read(fd, buf, 1);
-			if (bytes == 0)
-				break ;
-			if (bytes == -1)
-				return ((t_map_info){-1, -1, NULL, -1});
-		}
-		if (i == 0)
-			return (print_error(MAP_NL_ERR), (t_map_info){.x = 0, .y = 0});
+		if (!try_read_map_line(fd, buf, &mi))
+			return ((t_map_info){.x = 0, .y = 0});
 		mi.x++;
 		if (read(fd, buf, 1) == 0)
 			break ;
